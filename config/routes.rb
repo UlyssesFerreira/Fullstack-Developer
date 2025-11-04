7f537b6bbd2618d806f688261e947a464b9e1df9
UlyssesFerreira <ulyssesh.20@gmail.com>
2025-11-04 10:13:11 -0300

    user use cases

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "/profile", to: "user#show"
+  resource :user, only: [ :update, :destroy ] do
+    get "/profile", to: "users#edit", as: :profile, on: :collection
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
