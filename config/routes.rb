91a46a924e3b0132ed21abe0879215334d3b682a
UlyssesFerreira <ulyssesh.20@gmail.com>
2025-11-04 10:20:22 -0300

    created admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resource :user, only: [ :update, :destroy ] do
     get "/profile", to: "users#edit", as: :profile, on: :collection
   end
 
+  namespace :admin do
+    resource :dashboard, only: [ :show ]
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
+
+  root "admin/dashboards#show"
 end
